7086f6f11efbe40785b3b2caa5b34892bf0f13fa
BhaveshS03 <bhaveshsonar003@gmail.com>
2025-07-24 19:24:54 +0530

    fix type

--- headers/bus.hpp
+++ headers/bus.hpp
@@ -13,8 +13,8 @@ class Bus {
         uint8_t hram[0x80];
         uint8_t hram_read(uint16_t address);
         uint8_t wram_read(uint16_t address);
-        uint8_t hram_write(uint16_t address, uint8_t value);
-        uint8_t wram_write(uint16_t address, uint8_t value);
+        void hram_write(uint16_t address, uint8_t value);
+        void wram_write(uint16_t address, uint8_t value);
     
     public:
         Bus(Cart cart);
